868018c40676d4f6e58a655b4429e66918e57190
Stew Esho <StewardEsho@gmail.com>
2018-05-31 11:36:37 -0400

    Added David's level, undo button

--- Assets/Scripts/ExtensionMethods.cs
+++ Assets/Scripts/ExtensionMethods.cs
@@ -11,4 +11,10 @@ public static class ExtensionMethods{
   public static Vector3 V3(this Vector2 vector){
     return new Vector3(vector.x, vector.y, 0);
   }
+
+  public static T Pop<T>(this List<T> list) {
+    T r = list[list.Count - 1];
+    list.RemoveAt(list.Count - 1);
+    return r;
+  }
 }
--- Assets/Scripts/Player/PlaceObjects.cs
+++ Assets/Scripts/Player/PlaceObjects.cs
@@ -7,29 +7,38 @@ public class PlaceObjects : MonoBehaviour {
   public GameObject obj;
   public float rotationSpeed = 5f;
   GameObject placed_object;
+  List<GameObject> placementStack;
 
 	// Use this for initialization
 	void Start () {
+    placementStack = new List<GameObject>();
 	}
 
 //new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10)
 
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetButtonDown("Fire1")){
+		if (Input.GetMouseButtonDown(0)){
       placed_object = Instantiate(obj,
           Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10)),
           Quaternion.identity) as GameObject;
+      placementStack.Add(placed_object);
     }
-    if (Input.GetButton("Fire1")){
+    if (Input.GetMouseButton(0)){
       if (placed_object != null){
         float zRotation = Input.GetAxis("Mouse Y") * rotationSpeed * Time.deltaTime;
         // placed_object.transform.eulerAngles = new Vector3 (0, 0, placed_object.transform.eulerAngles.z + zRotation);
         placed_object.transform.Rotate(0, 0, zRotation);
       }
     }
-    if (Input.GetButtonUp("Fire1")){
+    if (Input.GetMouseButtonUp(0)){
       placed_object = null;
+    }
+
+    if ((Input.GetKey(KeyCode.RightControl) || Input.GetKey(KeyCode.LeftControl)) && Input.GetKeyDown(KeyCode.Z) && placementStack.Count > 0){
+      Debug.Log(placementStack);
+      GameObject objectToRemove = placementStack.Pop();
+      Destroy(objectToRemove);
     }
 	}
 }
