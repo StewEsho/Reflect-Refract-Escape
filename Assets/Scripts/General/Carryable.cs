caca41722e296721830e85f730ef526130630f03
Stew Esho <StewardEsho@gmail.com>
2018-07-09 11:39:52 -0400

    Objects can now be picked up, moved around, and placed down

---
+++ Assets/Scripts/General/Carryable.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+/**
+ * To be added to an empty game object as a CHILD of the carryable item.
+ * This object will contain the trigger that acts as the range for picking objects up.
+ */
+public class Carryable : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.transform.CompareTag("Player"))
+		{
+			PlaceObjects po = other.gameObject.GetComponent<PlaceObjects>();
+			if (po.GetState() == "Standard")
+				po.AddCarryInRange(transform.parent.gameObject);
+		}
+	}
+	
+	void OnTriggerExit2D(Collider2D other)
+	{
+		if (other.transform.CompareTag("Player"))
+		{
+			PlaceObjects po = other.gameObject.GetComponent<PlaceObjects>();
+			if (po.GetState() == "Standard")
+				po.RemoveCarryInRange();
+		}
+	}
+}
--- Assets/Scripts/Player/PlaceObjects.cs
+++ Assets/Scripts/Player/PlaceObjects.cs
@@ -6,6 +6,13 @@ using System;
 public class PlaceObjects : MonoBehaviour
 {
 
+    enum State
+    {
+        Standard,
+        PlaceMode,
+        Carrying
+    };
+    
     [SerializeField]
     private string id; //"P1" or "P2"
     [SerializeField]
@@ -19,12 +26,15 @@ public class PlaceObjects : MonoBehaviour
     private float controllerDeadzone = 0.19f;
 
     private int objectIndex = 0;
-    private List<GameObject> placementStack;
-    private bool inPlaceMode = false;
+    private List<GameObject> placementStack; //TODO: check to see if placement stack is even needed.
+    private State state = State.Standard; //State for whether in place mode or carrying optic or neither, etc. //TODO: needed???
+    private bool inPlaceMode = false; //true when inside of place mode
+    private bool isCarrying = false; //true when carrying an optic
+    private GameObject carryInRange, carry; //object in range to be carried, and object that is actually being carried.
     private Vector2 ghostPlacementDir;
 
     //UI for selecting and placing items
-    private SpriteRenderer selector;
+    private SpriteRenderer selector; //TODO: move this ui and tooltips to a seperate PlayerUI Script
     private Transform ghostPositioner; //a positioner which is moved and rotated, while the selected ghost matches its transform.
 
     void Start()
@@ -45,38 +55,98 @@ public class PlaceObjects : MonoBehaviour
             TogglePlaceMode();
         }
 
-        //Controls for when in place mode
-        if (inPlaceMode)
+        if (state == State.PlaceMode || state == State.Carrying)
         {
-            //Press A to place selected object
-            if (Input.GetButtonDown("Place_" + id))
+            //Controls for when in place mode
+            if (state == State.PlaceMode)
             {
-                placementStack.Add(Instantiate(objects[objectIndex].gameObject, ghostPositioner.position, ghostPositioner.rotation));
-            }
+                //Press A to place selected object
+                if (Input.GetButtonDown("Place_" + id))
+                {
+                    placementStack.Add(Instantiate(objects[objectIndex].gameObject, ghostPositioner.position, ghostPositioner.rotation));
+                }
             
-            //Use bumpers to scroll through items
-            if (Input.GetButtonDown("SelectNext_" + id))
+                //Use bumpers to scroll through items
+                if (Input.GetButtonDown("SelectNext_" + id))
+                {
+                    SelectNewObject(objectIndex + 1);
+                }
+                if (Input.GetButtonDown("SelectPrev_" + id))
+                {
+                    SelectNewObject(objectIndex - 1);
+                }
+                
+                //move positioner with right stick
+                ghostPlacementDir = new Vector2(Input.GetAxis("RightJoystickX_" + id), -Input.GetAxis("RightJoystickY_" + id));
+                if(ghostPlacementDir.sqrMagnitude > controllerDeadzone)
+                    ghostPositioner.localPosition = ghostPlacementDir.normalized * ghostDistance;
+            
+                //rotate positioner with triggers
+                float zRotation = Input.GetAxis("Triggers_" + id) * rotationSpeed * Time.deltaTime;
+                ghostPositioner.transform.Rotate(0, 0, zRotation);
+                
+                //Ensure ghost's transform matches the positioner's transform
+                ghosts[objectIndex].transform.position = ghostPositioner.position;
+                ghosts[objectIndex].transform.rotation = ghostPositioner.rotation;
+            } 
+            else if (state == State.Carrying)
             {
-                SelectNewObject(objectIndex + 1);
+                try
+                {
+                    if (carry != null)
+                    {
+                        //move object with right stick
+                        Vector2 dir = new Vector2(Input.GetAxis("RightJoystickX_" + id), -Input.GetAxis("RightJoystickY_" + id));
+                        if(dir.sqrMagnitude > controllerDeadzone)
+                            carry.transform.localPosition = dir.normalized * ghostDistance;
+            
+                        //rotate object with triggers
+                        float zRotation = Input.GetAxis("Triggers_" + id) * rotationSpeed * Time.deltaTime;
+                        carry.transform.Rotate(0, 0, zRotation);
+                        
+                        //Press B to delete the object being carried.
+                        if (Input.GetButtonDown("Delete_" + id))
+                        {
+                            Destroy(carry);
+                            carry = null;
+                            state = State.Standard;
+                            //TODO: reduce mirror placed count (or not to make things more challenging maybe). Just take care of it.
+                        }
+                    
+                        //Press A to place down object being carried.
+                        if (Input.GetButtonDown("Place_" + id))
+                        {
+                            carry.transform.SetParent(null);
+                            carry = null; //By setting the reference to null, the object will no longer move around and thus be "placed".
+                            isCarrying = false;
+                            state = State.Standard;
+                            //TODO: enable carryable on object
+                        }
+                    }
+                    
+                    
+                    //Ensure ghost's transform matches the positioner's transform
+//                    carry.transform.position = ghostPositioner.position;
+//                    carry.transform.rotation = ghostPositioner.rotation;
+                }
+                catch (NullReferenceException e)
+                {
+                    Debug.LogException(e, this);
+                }
             }
-            if (Input.GetButtonDown("SelectPrev_" + id))
-            {
-                SelectNewObject(objectIndex - 1);
-            }
-            
-            //rotate positioner with triggers
-            float zRotation = Input.GetAxis("Triggers_" + id) * rotationSpeed * Time.deltaTime;
-            ghostPositioner.transform.Rotate(0, 0, zRotation);
-            
-            //move positioner with right stick
-            ghostPlacementDir = new Vector2(Input.GetAxis("RightJoystickX_" + id), -Input.GetAxis("RightJoystickY_" + id));
-            if(ghostPlacementDir.sqrMagnitude > controllerDeadzone)
-                ghostPositioner.localPosition = ghostPlacementDir.normalized * ghostDistance;
-            
-            //Ensure ghost's transform matches the positioner's transform
-            ghosts[objectIndex].transform.position = ghostPositioner.position;
-            ghosts[objectIndex].transform.rotation = ghostPositioner.rotation;
         }
+        else if (state == State.Standard) //Not placing nor carrying an object
+        {
+            if (carryInRange != null && Input.GetButtonDown("Place_" + id))
+            {
+                carry = carryInRange;
+                state = State.Carrying;
+                carryInRange = null;
+                carry.transform.SetParent(transform, true); //parent the object;
+                //TODO: disable carryable from object
+            }
+        }
+        
     }
 
     public void CreateGhosts()
@@ -87,7 +157,7 @@ public class PlaceObjects : MonoBehaviour
         foreach (PlaceableItem obj in objects)
         {
             //Instansiate ghost as child
-            GameObject ghost = Instantiate(obj.gameObject, ghostPositioner.position, ghostPositioner.rotation, this.transform);
+            GameObject ghost = Instantiate(obj.gameObject, ghostPositioner.position, ghostPositioner.rotation, transform);
             //Add GhostOptic script, which will continue the process of instansiating the ghost
             ghost.AddComponent<GhostOptic>();
             //Add ghost to list of ghosts;
@@ -97,14 +167,18 @@ public class PlaceObjects : MonoBehaviour
 
     public void ClearGhosts()
     {
-        
+        //TODO: Implement PlaceObjects.ClearGhosts()
     }
 
     public void TogglePlaceMode()
     {
-        inPlaceMode = !inPlaceMode;
-        ghosts[objectIndex].SetActive(inPlaceMode);
-        selector.transform.parent.gameObject.SetActive(inPlaceMode);
+        if (state != State.Carrying)
+        {
+            inPlaceMode = !inPlaceMode;
+            state = inPlaceMode ? State.PlaceMode : State.Standard;
+            ghosts[objectIndex].SetActive(inPlaceMode);
+            selector.transform.parent.gameObject.SetActive(inPlaceMode);
+        }
     }
 
     public void SelectNewObject(int index)
@@ -115,6 +189,21 @@ public class PlaceObjects : MonoBehaviour
         objectIndex = (int) Mathf.Repeat(index, ghosts.Count);
         ghosts[objectIndex].SetActive(true);
         selector.sprite = objects[objectIndex].thumbnail;
+    }
+
+    public void AddCarryInRange(GameObject go)
+    {
+        carryInRange = carryInRange == null ? go : carryInRange;
+    }
+
+    public void RemoveCarryInRange()
+    {
+        carryInRange = null;
+    }
+
+    public string GetState()
+    {
+        return state.ToString();
     }
 
 }
