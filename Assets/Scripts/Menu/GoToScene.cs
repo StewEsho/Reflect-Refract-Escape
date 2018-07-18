241d9049cbb681818a77062f5673071fbbe24a9f
Stew Esho <StewardEsho@gmail.com>
2018-07-18 18:52:27 -0400

    added new levels

---
+++ Assets/Scripts/Menu/GoToScene.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoToScene : MonoBehaviour {
+
+	public void LoadLevel(int id)
+	{
+		SceneManager.LoadScene(id);
+	}
+
+	public void LoadNextLevel()
+	{
+		SceneManager.LoadScene((int)Mathf.Repeat(SceneManager.GetActiveScene().buildIndex + 1, SceneManager.sceneCountInBuildSettings));
+	}
+}
