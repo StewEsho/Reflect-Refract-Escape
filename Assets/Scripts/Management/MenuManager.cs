d9daaacb09cb5554fdfbac935e0831ad0bd5d201
Stew Esho <StewardEsho@gmail.com>
2018-07-26 13:09:14 -0400

    today

---
+++ Assets/Scripts/Management/MenuManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuManager : MonoBehaviour
+{
+	private Button playButton;
+
+	// Use this for initialization
+	void Start ()
+	{
+		playButton = transform.Find("Button").GetComponent<Button>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("A_P1") || Input.GetButtonDown("A_P2"))
+		{
+			playButton.onClick.Invoke();
+		}
+	}
+}
