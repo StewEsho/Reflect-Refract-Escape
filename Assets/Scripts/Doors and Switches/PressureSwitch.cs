01bf473cb8d732a8f9bb12f10037c35fe2e2bc4d
Stew Esho <StewardEsho@gmail.com>
2018-07-30 20:14:05 -0400

    added presure switches

---
+++ Assets/Scripts/Doors and Switches/PressureSwitch.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public class PressureSwitch : MonoBehaviour
+{
+
+	public BeamEmitter beam;
+	public bool isEnabled = false;
+
+	void Start()
+	{
+		beam.IsActive = isEnabled;
+	}
+
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.transform.CompareTag("Player"))
+		{
+			isEnabled = !isEnabled;
+		}
+		
+		beam.IsActive = isEnabled;
+	}
+}
