1e085475807049bc98fd652601365f7aa89c9bc2
Stew Esho <StewardEsho@gmail.com>
2018-08-13 00:15:13 -0400

    lenses work!

--- Assets/Scripts/Light Physics/BeamEmitter.cs
+++ Assets/Scripts/Light Physics/BeamEmitter.cs
@@ -104,16 +104,9 @@ public class BeamEmitter : MonoBehaviour
         var hits = Physics2D.RaycastAll(origin, dir, distance).OrderBy(h => h.distance).ToList();
         RaycastHit2D hit = hits[0];
         //ignore lens.
-        if (fromLens)
+        if (fromLens && hits.Count > 1 && hits[0].transform.CompareTag("Lens"))
         {
-            for (int i = 0; i < Math.Min(hits.Count, 3); i++)
-            {
-                if (!hits[i].transform.CompareTag("Lens"))
-                {
-                    hit = hits[i];
-                    break;
-                }
-            }
+            hit = hits[1];
         }
         Debug.Log(hit.transform.tag);
 
